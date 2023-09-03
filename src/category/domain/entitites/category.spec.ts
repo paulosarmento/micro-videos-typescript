e7bbfe649c888dc686ddb8a0e819fd0ed3d3f98c
Mac <mac@MacBook-Pro-de-Mac.local>
2023-09-02 23:53:01 -0300

    Instalando e configurando Jest

---
+++ src/category/domain/entitites/category.spec.ts
@@ -0,0 +1,12 @@
+import { Category } from "./category"
+
+describe('Category Tests', () => {
+
+  test('constructor category', () => {
+    const category : Category = new Category('Movie')
+    expect(category.name).toBe('Movie')
+  })
+
+
+
+})

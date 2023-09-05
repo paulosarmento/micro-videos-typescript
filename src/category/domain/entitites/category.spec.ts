81eca08a742333f15ff9e51882155e56a6d4cf1c
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-09-05 11:13:38 -0300

    Teste constructor da entidade categoria

--- src/category/domain/entitites/category.spec.ts
+++ src/category/domain/entitites/category.spec.ts
@@ -2,7 +2,22 @@ import { Category } from "./category";
 
 describe("Category Unit Tests", () => {
   test("constructor category", () => {
-    const category = new Category({ name: "Movie" });
-    expect(category.name).toBe("Movie");
+    const created_at = new Date();
+    const category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
+    // expect(category.name).toBe("Movie");
+    // expect(category.description).toBe("some description");
+    // expect(category.is_active).toBeTruthy();
+    // expect(category.created_at).toBe(created_at);
   });
 });

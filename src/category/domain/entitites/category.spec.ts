88504605e8cb288a3068b83b285d6e7fff722a80
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-09-05 14:53:52 -0300

    Teste lógica de atribuição das propriedades facultativas

--- src/category/domain/entitites/category.spec.ts
+++ src/category/domain/entitites/category.spec.ts
@@ -1,20 +1,58 @@
 import { Category } from "./category";
+import { omit } from "lodash";
 
 describe("Category Unit Tests", () => {
   test("constructor category", () => {
-    const created_at = new Date();
-    const category = new Category({
+    let category = new Category({ name: "Movie" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
       name: "Movie",
       description: "some description",
-      is_active: true,
-      created_at,
+      is_active: false,
     });
+    let created_at = new Date();
     expect(category.props).toStrictEqual({
       name: "Movie",
       description: "some description",
-      is_active: true,
+      is_active: false,
       created_at,
     });
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "some description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+
     // expect(category.name).toBe("Movie");
     // expect(category.description).toBe("some description");
     // expect(category.is_active).toBeTruthy();
--- src/category/domain/entitites/category.ts
+++ src/category/domain/entitites/category.ts
@@ -6,17 +6,29 @@ export type CategoryProperties = {
 };
 
 export class Category {
-  constructor(public readonly props: CategoryProperties) {}
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description ?? null;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
 
   get name() {
     return this.props.name;
   }
 
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
   get description() {
     return this.props.description;
   }
+
   get is_active() {
     return this.props.is_active;
+  }
+  private set is_active(value: boolean) {
+    this.props.is_active = this.props.is_active ?? true;
   }
 
   get created_at() {

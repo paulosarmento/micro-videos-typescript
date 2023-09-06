bf8bf092df754dc83fcd642fdf076b813de8cdb2
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-09-05 21:29:25 -0300

    Teste Objeto de Valor UniqueEntityId parte 2

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -7,4 +7,18 @@ describe("UniqueEntityId unit Tests", () => {
     expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
     expect(validateSpy).toHaveBeenCalled();
   });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const uuid = "6d5c69ad-5ae7-412d-90c4-1e0f42c7af01";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const vo = new UniqueEntityId();
+    expect(vo.id).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
 });
